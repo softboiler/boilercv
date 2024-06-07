7d0935c025cb1c87014d1ed632c99708d94f2d32
blakeNaccarato <blake.naccarato@gmail.com>
2024-06-07 16:05:22 -0700

    Plot latest correlations

--- pipeline/boilercv_pipeline/equations/make_docs.py
+++ pipeline/boilercv_pipeline/equations/make_docs.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from textwrap import dedent
 from tomllib import loads
 
+from boilercv.correlations import GROUPS
 from boilercv.correlations.models import Equations, prep_equation_forms
 from boilercv.correlations.types import Corr
 from boilercv_pipeline.equations import EQUATIONS, SYMS, get_raw_equations_context
@@ -15,19 +16,30 @@ def main():  # noqa: D103
     corr: Corr = "beta"
     equations_path = EQUATIONS[corr]
     content: str = ""
-    equations = Equations[str].context_model_validate(
-        obj=loads(equations_path.read_text("utf-8") if equations_path.exists() else ""),
-        context=get_raw_equations_context(symbols=SYMS[corr]),
+    equations = (
+        Equations[str]
+        .context_model_validate(
+            obj=loads(
+                equations_path.read_text("utf-8") if equations_path.exists() else ""
+            ),
+            context=get_raw_equations_context(symbols=SYMS[corr]),
+        )
+        .model_dump()
     )
-    for name, eq in equations.items():
-        eq = prep_equation_forms(eq.model_dump())["latex"]  # pyright: ignore[reportArgumentType]
-        content += dedent(f"""
-            ## {name}
+    # TODO: Fix spacing and incorporate heading details
+    for group in ["Group 1", "Group 3", "Group 4", "Group 5"]:
+        content += f"## {group}\n"
+        for name, eq in equations.items():
+            if GROUPS[name] == group:
+                eq = prep_equation_forms(equations[name])["latex"]  # pyright: ignore[reportArgumentType]
+                content += dedent(f"""
+                    ### {name}
 
-            $$
-            {eq}
-            $$ (eq_{name})
-            """)
+                    $$
+                    {eq}
+                    $$ (eq_dimensionless_bubble_diameter_{name})
+
+                    """)
     PATH.write_text(encoding="utf-8", data=content.lstrip())
 
 
--- src/boilercv/correlations/__init__.py
+++ src/boilercv/correlations/__init__.py
@@ -10,3 +10,22 @@ SYMBOLS = {
     "alpha": "thermal_diffusivity",
     "pi": "pi",
 }
+
+GROUPS = {
+    k: f"Group {v}"
+    for k, v in {
+        "florschuetz_chao_1965": 1,
+        "isenberg_sideman_1970": 3,
+        "akiyama_1973": 3,
+        "chen_mayinger_1992": 3,
+        "zeitoun_et_al_1995": 5,
+        "kalman_mori_2002": 3,
+        "warrier_et_al_2002": 4,
+        "yuan_et_al_2009": 4,
+        "lucic_mayinger_2010": 3,
+        "kim_park_2011": 3,
+        "inaba_et_al_2013": 5,
+        "al_issa_et_al_2014": 3,
+        "tang_et_al_2016": 3,
+    }.items()
+}
