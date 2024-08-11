878fc1045b60f5f2094f08f3e7d522f2045db29f
blakeNaccarato <blake.naccarato@gmail.com>
2024-08-10 21:41:40 -0700

    Add remaining stages to CLI

--- pipeline/boilercv_pipeline/cli.py
+++ pipeline/boilercv_pipeline/cli.py
@@ -7,6 +7,17 @@ from cappa.subcommand import Subcommands
 
 from boilercv_pipeline.stages.binarize import Binarize
 from boilercv_pipeline.stages.convert import Convert
+from boilercv_pipeline.stages.e230920_find_contours import E230920FindContours
+from boilercv_pipeline.stages.e230920_find_objects import E230920FindObjects
+from boilercv_pipeline.stages.e230920_find_tracks import E230920FindTracks
+from boilercv_pipeline.stages.e230920_get_mae import E230920GetMae
+from boilercv_pipeline.stages.e230920_merge_mae import E230920MergeMae
+from boilercv_pipeline.stages.e230920_merge_tracks import E230920MergeTracks
+from boilercv_pipeline.stages.e230920_plot_tracks import E230920PlotTracks
+from boilercv_pipeline.stages.e230920_process_tracks import E230920ProcessTracks
+from boilercv_pipeline.stages.e230920_update_thermal_data import (
+    E230920UpdateThermalData,
+)
 from boilercv_pipeline.stages.fill import Fill
 from boilercv_pipeline.stages.find_contours import FindContours
 from boilercv_pipeline.stages.flatten_data_dir import FlattenDataDir
@@ -32,6 +43,15 @@ class Stages:
     commands: Subcommands[
         Binarize
         | Convert
+        | E230920FindContours
+        | E230920FindObjects
+        | E230920FindTracks
+        | E230920GetMae
+        | E230920MergeMae
+        | E230920MergeTracks
+        | E230920PlotTracks
+        | E230920ProcessTracks
+        | E230920UpdateThermalData
         | Fill
         | FindContours
         | FlattenDataDir
