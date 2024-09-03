f3c3a7dd5a10eb48e0451a8c14d41788d2fc7a73
blakeNaccarato <blake.naccarato@gmail.com>
2024-09-02 17:16:31 -0700

    Remove unused dependency from pipeline

--- pipeline/boilercv_pipeline/config.py
+++ pipeline/boilercv_pipeline/config.py
@@ -3,14 +3,7 @@
 from pathlib import Path
 
 from boilercore.paths import get_package_dir, map_stages
-from boilercore.settings_models import (
-    Paths,
-    customise_sources,
-    get_settings_paths,
-    sync_settings_schema,
-)
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
 
 import boilercv_pipeline
 
@@ -18,8 +11,6 @@ import boilercv_pipeline
 class Constants(BaseModel, use_attribute_docstrings=True):
     """Constants."""
 
-    settings_paths: Paths = get_settings_paths(boilercv_pipeline)
-    """Settings paths."""
     package_dir: Path = get_package_dir(boilercv_pipeline)
     """Package directory."""
     stages: dict[str, Path] = {
@@ -34,50 +25,3 @@ class Constants(BaseModel, use_attribute_docstrings=True):
 
 const = Constants()
 """Constants."""
-
-
-class PluginModelConfig(BaseSettings, use_attribute_docstrings=True):
-    """Pydantic plugin model configuration."""
-
-    @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[BaseSettings],
-        init_settings: PydanticBaseSettingsSource,
-        *_args: PydanticBaseSettingsSource,
-        **_kwds: PydanticBaseSettingsSource,
-    ) -> tuple[PydanticBaseSettingsSource, ...]:
-        """Source settings from init and TOML."""
-        return customise_sources(
-            settings_cls, init_settings, const.settings_paths.plugin_settings
-        )
-
-
-class Settings(BaseSettings, use_attribute_docstrings=True):
-    """Package settings."""
-
-    @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[BaseSettings],
-        init_settings: PydanticBaseSettingsSource,
-        *_args: PydanticBaseSettingsSource,
-        **_kwds: PydanticBaseSettingsSource,
-    ) -> tuple[PydanticBaseSettingsSource, ...]:
-        """Source  settings from init and TOML."""
-        return customise_sources(
-            settings_cls, init_settings, const.settings_paths.settings
-        )
-
-
-for path, model in zip(
-    const.settings_paths.all_dev_settings
-    if const.settings_paths.in_dev
-    else const.settings_paths.all_cwd_settings,
-    (PluginModelConfig, Settings),
-    strict=True,
-):
-    sync_settings_schema(path, model)
-
-
-default = Settings()
