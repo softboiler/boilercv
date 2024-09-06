3d698790b95eac331337eb9ced9168724a5941a8
blakeNaccarato <blake.naccarato@gmail.com>
2024-09-06 00:58:59 -0700

    Update DAG

--- docs/boilercv_docs/models/__init__.py
+++ docs/boilercv_docs/models/__init__.py
@@ -12,17 +12,15 @@ class Build(BaseModel):
     mode: BuildMode = None
     """Force building documentation in a certain mode."""
     nb_execution_excludepatterns: NbExecutionExcludePatterns = [
-        "notebooks/e230920*.ipynb",
-        "notebooks/find_objects.ipynb",
+        "notebooks/e230920*.ipynb"
     ]
     """List of directories relative to `docs` to exclude executing notebooks in."""
-    # TODO: Set to `cache` after fixing notebooks
-    nb_execution_mode: NbExecutionMode = "cache"
+    nb_execution_mode: NbExecutionMode = "off"
     """Notebook execution mode.
 
     https://myst-nb.readthedocs.io/en/stable/computation/execute.html#notebook-execution-modes
     """
-    skip_autodoc: SkipAutodoc = True
+    skip_autodoc: SkipAutodoc = False
     """Skip the potentially slow process of autodoc generation."""
     skip_autodoc_post_parse: bool = False
     """Skip the potentially slow process of autodoc post parsing."""
