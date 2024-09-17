237b74e7c9c6f597c89bd8a999ab718ed9ba1c35
blakeNaccarato <blake.naccarato@gmail.com>
2024-09-17 11:12:12 -0700

    Sync with template

--- scripts/Initialize-Shell.ps1
+++ scripts/Initialize-Shell.ps1
@@ -27,8 +27,30 @@ function Set-Env {
                 Find-Pattern '^([^.]+\.[^.]+).*$')
     )
 
-    # ? Sync the virtual environment
+    # ? Track environment variables to update `.env` with later
+    $EnvVars = @{}
+    $Sep = $IsWindows ? ';' : ':'
+    $EnvPath = $Env:GITHUB_ENV ? $Env:GITHUB_ENV : '.env'
+    # ? Create `env` if missing
+    if (!($EnvFile = Get-Item $EnvPath -ErrorAction 'Ignore')) {
+        New-Item $EnvPath
+        $EnvFile = Get-Item $EnvPath
+    }
+    # ? Create local `bin` if missing
+    if (!($Bin = Get-Item 'bin' -ErrorAction 'Ignore')) {
+        New-Item 'bin' -ItemType 'Directory'
+        $Bin = Get-Item 'bin'
+    }
+    # ? Add local `bin` to path
+    $Path = $Env:PATH = "$Bin$Sep$Env:PATH"
+    # ? Set `uv` tool directory to local `bin`
+    $Env:UV_TOOL_BIN_DIR = $Bin
+    $EnvVars.Add('UV_TOOL_BIN_DIR', $Bin)
+
+    # ? Sync local `uv` version
     Sync-Uv
+
+    # ? Sync contributor virtual environment
     $CI = $Env:SYNC_PY_DISABLE_CI ? $null : $Env:CI
     if (!$CI) {
         if (!(Test-Path '.venv')) { uv venv --python $Version }
@@ -43,39 +65,42 @@ function Set-Env {
     }
     if (!(Get-Command 'boilercv_tools' -ErrorAction 'Ignore')) {
         'Installing tools' | Write-Progress
-        $Env:UV_TOOL_BIN_DIR = Get-Item 'bin'
-        uv tool install --force --python $Version --resolution 'lowest-direct' 'scripts/.'
+        uv tool install --force --python $Version --resolution 'lowest-direct' --editable 'scripts/.'
         'Tools installed' | Write-Progress -Done
     }
 
-    # ? Set environment variables
-    $EnvVars = @{}
+    # ? Get environment variables from `pyproject.toml`
     boilercv_tools init-shell |
         Select-String -Pattern '^(.+)=(.+)$' |
         ForEach-Object {
-            $EnvVars.Add($_.Matches.Groups[1].Value, $_.Matches.Groups[2].Value)
+            $Key, $Value = $_.Matches.Groups[1].Value, $_.Matches.Groups[2].Value
+            if ((($Key.ToLower() -ne 'path')) -and ($EnvVars -notcontains $Key)) {
+                $EnvVars.Add($Key, $Value)
+            }
         }
+    # ? Get environment variables to update in `.env`
     $Keys = @()
-    $EnvFile = $Env:GITHUB_ENV ? $Env:GITHUB_ENV : '.env'
-    if (!(Test-Path $EnvFile)) { New-Item $EnvFile }
     $Lines = Get-Content $EnvFile | ForEach-Object {
         $_ -replace '^(?<Key>.+)=(?<Value>.+)$', {
             $Key = $_.Groups['Key'].Value
-            if ($EnvVars.ContainsKey($Key)) {
+            if ($Key.ToLower() -eq 'path') { $PathInEnvFile = $true }
+            elseif ($EnvVars.ContainsKey($Key)) {
                 $Keys += $Key
                 return "$Key=$($EnvVars[$Key])"
             }
             return $_
         }
     }
+    # ? Sync environment variables and those in `.env`
     $NewLines = $EnvVars.GetEnumerator() | ForEach-Object {
         $Key, $Value = $_.Key, $_.Value
-        Set-Item "Env:$Key" $Value
-        if (($Key.ToLower() -ne 'path') -and ($Keys -notcontains $Key)) {
-            return "$Key=$Value"
+        if ($Key.ToLower() -ne 'path') {
+            Set-Item "Env:$Key" $Value
+            if ($Keys -notcontains $Key) { return "$Key=$Value" }
         }
     }
     @($Lines, $NewLines) | Set-Content $EnvFile
+    if ($CI -and !$PathInEnvFile) { "PATH=$Path" | Add-Content $EnvFile }
 }
 
 Set-Env
