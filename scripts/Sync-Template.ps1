08b4ace6062e6f208d52c7202be299600b7f8d67
blakeNaccarato <blake.naccarato@gmail.com>
2024-09-18 12:45:53 -0700

    Fix template script

--- scripts/Sync-Template.ps1
+++ scripts/Sync-Template.ps1
@@ -13,17 +13,17 @@ Param(
 begin {
     . scripts/Common.ps1
     $Copier = 'copier@9.2.0'
-    $Ref = $Ref ? $Ref : (Get-Content '.copier-answers.yml' | Find-Pattern '^_commit:\s.+([^-]+)$')
 }
 process {
     if (!$Stay) {
         git submodule update --init --remote --merge $Template
         git add .
-        try { git commit --no-verify -m "Update template digest to $(Get-Ref $Ref)" }
+        $Ref = $Ref ? $Ref : (Get-Content '.copier-answers.yml' | Find-Pattern '^_commit:\s.+([^-]+)$')
+        try { git commit --no-verify -m "Update template digest to $Ref" }
         catch [System.Management.Automation.NativeCommandExitException] { $AlreadyUpdated = $true }
     }
     # ? Get latest ref
-    $Ref = Get-Ref
+    $Ref = $Ref ? $Ref : (Get-Content '.copier-answers.yml' | Find-Pattern '^_commit:\s.+([^-]+)$')
     if ($Recopy) {
         if ($Prompt) { return uvx $Copier $Subcommand $Defaults --vcs-ref=$Ref }
         return uvx $Copier recopy --overwrite --defaults --vcs-ref=$Ref
