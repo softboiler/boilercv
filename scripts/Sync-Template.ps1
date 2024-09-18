ab35a2b5e5b837e5211170fbd666e558d94d7388
blakeNaccarato <blake.naccarato@gmail.com>
2024-09-18 16:47:44 -0700

    Template versions

--- scripts/Sync-Template.ps1
+++ scripts/Sync-Template.ps1
@@ -2,7 +2,7 @@
 Sync with template.#>
 Param(
     # Specific template VCS reference.
-    [Parameter(ValueFromPipeline)]$Ref,
+    [string]$Ref,
     # Prompt for new answers.
     [switch]$Prompt,
     # Recopy, ignoring prior diffs instead of a smart update.
@@ -10,24 +10,23 @@ Param(
     # Stay on the current template version when updating.
     [switch]$Stay
 )
-begin {
-    . scripts/Common.ps1
-    $Copier = 'copier@9.2.0'
-}
-process {
-    if (!$Stay) {
-        git submodule update --init --remote --merge $Template
-        git add .
-        $Ref = $Ref ? $Ref : (Get-Content '.copier-answers.yml' | Find-Pattern '^_commit:\s.+([^-]+)$')
-        try { git commit --no-verify -m "Update template digest to $Ref" }
-        catch [System.Management.Automation.NativeCommandExitException] { $AlreadyUpdated = $true }
-    }
-    # ? Get latest ref
+
+. scripts/Common.ps1
+
+$Copier = 'copier@9.2.0'
+
+if (!$Stay) {
+    git submodule update --init --remote --merge $Template
+    git add .
     $Ref = $Ref ? $Ref : (Get-Content '.copier-answers.yml' | Find-Pattern '^_commit:\s.+([^-]+)$')
-    if ($Recopy) {
-        if ($Prompt) { return uvx $Copier $Subcommand $Defaults --vcs-ref=$Ref }
-        return uvx $Copier recopy --overwrite --defaults --vcs-ref=$Ref
-    }
-    if ($Prompt) { return uvx $Copier update --vcs-ref=$Ref }
-    return uvx $Copier update --defaults --vcs-ref=$Ref
+    try { git commit --no-verify -m "Update template digest to $Ref" }
+    catch [System.Management.Automation.NativeCommandExitException] { $AlreadyUpdated = $true }
 }
+# ? Get latest ref
+$Ref = $Ref ? $Ref : (Get-Content '.copier-answers.yml' | Find-Pattern '^_commit:\s.+([^-]+)$')
+if ($Recopy) {
+    if ($Prompt) { return uvx $Copier $Subcommand $Defaults --vcs-ref=$Ref }
+    return uvx $Copier recopy --overwrite --defaults --vcs-ref=$Ref
+}
+if ($Prompt) { return uvx $Copier update --vcs-ref=$Ref }
+return uvx $Copier update --defaults --vcs-ref=$Ref
